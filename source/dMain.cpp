a2eb81afbe421e4177d02fe0b91ed86aa8c69044
Luís F. Loureiro <nczeroshift.dev@gmail.com>
2016-01-29 00:33:03 +0000

    xernobyl demo rough implementation

--- source/dMain.cpp
+++ source/dMain.cpp
@@ -20,6 +20,224 @@
 #include "dSettings.h"
 #include "dSubtitles.h"
 
+/*
+'use strict'
+
+let frame = 0
+let canvas = null
+let context = null
+let particles = [[], []]
+let last_time = 0.0
+let sprite = null
+
+class vec2 {
+constructor(x, y) {
+this.v = new Float32Array(2)
+this.v[0] = x
+this.v[1] = y
+}
+
+get x() {
+return this.v[0]
+}
+
+get y() {
+return this.v[1]
+}
+}*/
+/*
+function rgba(r, g, b, a) {
+    return 'rgba(' + Math.round(r * 255.0) + ',' +
+        Math.round(g * 255.0) + ',' +
+        Math.round(b * 255.0) + ',' +
+        a + ')'
+}
+
+function createImage(width, height, render) {
+    let canvas = document.createElement('canvas')
+        canvas.width = width
+        canvas.height = height
+        let context = canvas.getContext('2d')
+
+        render(canvas, context)
+
+        let image = new Image()
+        image.src = canvas.toDataURL('image/png')
+        return image
+}
+
+function createParticleSprite(size, color_i, color_f) {
+    return createImage(size, size, (canvas, context) = > {
+        let gradient = context.createRadialGradient(size * 0.5, size * 0.5, size * 0.5, size * 0.5, size * 0.5, 0.0);
+        gradient.addColorStop(0, color_f)
+            gradient.addColorStop(1, color_i)
+            context.fillStyle = gradient
+            context.fillRect(0, 0, size, size)
+    })
+}
+*/
+class Canvas {
+public:
+    Canvas(float w, float h) {
+        width = w;
+        height = h;
+    }
+    float width;
+    float height;
+};
+
+float sign(float v) {
+    if (v < 0.0)
+        return -1.0;
+    else if (v > 0.0)
+        return 1.0;
+    else
+        return 0.0;
+}
+
+class Particle {
+public:
+    Particle(Canvas canvas) {
+        x = Math::RandomValue() * canvas.width;
+        y = Math::RandomValue() * canvas.height;
+        life = Math::RandomValue() * 4000 + 2000;
+        i_life = 1.0 / life;
+
+        vx = Math::RandomValue() * 2.0 - 1.0;
+        vy = Math::RandomValue() * 2.0 - 1.0;
+
+        i_size = Math::RandomValue() * 10.0 + 45.0;
+    }
+
+    void update(float dt, Canvas canvas) {
+        float v = life * i_life;
+
+        life -= dt;
+        size = v * i_size;
+
+        v *= v;
+
+        float cx = (x - canvas.width * 0.5) / canvas.height;
+        float cy = (y - canvas.height * 0.5) / canvas.height;
+
+        y += vy * dt * v * 0.125 + cy * cy * sign(cy) * 4.0;
+        x += vx * dt * v * 0.125 + cx * cx * sign(cx) * 4.0;
+    }
+
+   void render(Gui::ShapeRenderer * renderer) {
+       float a = 1.0 - life * i_life;
+       if (a < 0.0)
+           a = 0.0;
+       if (a > 1.0)
+           a = 1.0;
+       renderer->Square(x - 0.5 * size, y - 0.5 * size, size, size, Math::Color4ub(255, 255, 255, a * 255));
+    }
+
+    static void updateParticles(Canvas canvas, std::vector<Particle> * particles, int frame, float dt) {
+        int buffer = frame & 1;
+        int other_buffer = buffer ^ 1;
+
+        particles[buffer].clear();
+
+        for (int i = 0; i < 10; ++i) {
+            Particle p(canvas);
+            particles[buffer].push_back(p);
+        }
+        
+        for (int i = 0; i < particles[other_buffer].size();i++) {
+            particles[other_buffer][i].update(dt, canvas);
+            if (particles[other_buffer][i].life >= 0.0)
+                particles[buffer].push_back(particles[other_buffer][i]);
+        }
+    }
+
+
+    static void renderParticles(Gui::ShapeRenderer * renderer, int frame, std::vector<Particle> * particles) {
+        int buffer = frame & 1;
+        //context.save()
+        //context.globalCompositeOperation = 'lighter'
+
+        for (int i = 0; i < particles[buffer].size(); i++) {
+            particles[buffer][i].render(renderer);
+            //context.restore()
+        }
+    }
+
+
+    float x, y, life, i_life, size, vy,vx, i_size;
+};
+
+/*
+function updateParticles(dt) {
+    let buffer = frame & 1
+        let other_buffer = buffer ^ 1
+
+        particles[buffer] = []
+
+        for (let i = 0; i < 10; ++i) {
+            let p = new Particle()
+                particles[buffer].push(new Particle())
+        }
+
+
+    for (let i in particles[other_buffer]) {
+        particles[other_buffer][i].update(dt)
+            if (particles[other_buffer][i].life >= 0.0)
+                particles[buffer].push(particles[other_buffer][i])
+    }
+}
+
+function renderParticles() {
+    let buffer = frame & 1
+        //let other_buffer = buffer ^ 1
+
+        context.save()
+        context.globalCompositeOperation = 'lighter'
+
+        for (let i in particles[buffer])
+            particles[buffer][i].render()
+
+            context.restore()
+}
+
+function renderLoop(frame_time) {
+    let dt = frame_time - last_time
+        last_time = frame_time
+
+        updateParticles(dt)
+
+        let gradient = context.createRadialGradient(canvas.width * 0.5, 0.0, Math.sqrt(canvas.width * canvas.width + canvas.height * canvas.height), canvas.width * 0.5, canvas.height, 0.0);
+    gradient.addColorStop(0, 'rgba(0, 0, 0, 1.0)');
+    let l = (Math.sin(frame_time) * 0.05 + 1.0) + Math.random() * 0.0125
+        l *= 0.1
+        gradient.addColorStop(1, rgba(l * 0.75, l * 0.725, l, 1.0))
+
+        context.fillStyle = gradient
+
+        context.fillRect(0, 0, canvas.width, canvas.height)
+
+        renderParticles()
+
+        window.requestAnimationFrame(renderLoop)
+
+        ++frame
+}
+
+window.onload = function() {
+    canvas = document.getElementsByTagName('canvas')[0]
+        canvas.width = canvas.offsetWidth
+        canvas.height = canvas.offsetHeight
+
+        context = canvas.getContext('2d')
+        context.imageSmoothingEnabled = true
+
+        sprite = createParticleSprite(32, rgba(1.0, 1.0, 1.0, 1.0), rgba(1.0, 0.75, 0.5, 0.0))
+
+        renderLoop(0)
+}
+
+*/
+
 class GraphicRendering : public virtual Core::Threadable, public virtual Audio::DeviceCallback
 {
 public:
@@ -53,6 +271,8 @@ public:
 
     void Run() {
        
+        Graph::Program * particle = NULL;
+
         try {
             dev = Graph::CreateDevice(wnd, Graph::DEVICE_AUTO, wnd->GetWidth(), wnd->GetHeight());
             dev->ClearColor(0.0, 0.0, 0.0, 1.0);
@@ -73,6 +293,8 @@ public:
             fontMap->Load("script://sans_serif.txt");
             fontMap->SetPositionAccuracy(Gui::FONT_POSITION_ACCURACY_INTEGER);
 
+            particle = dev->LoadProgram("shader://particle.cpp");
+
             shpRender = new Gui::ShapeRenderer(dev);
         }
         catch (const Core::Exception & ex) {
@@ -90,18 +312,21 @@ public:
         s = subtitlesTimeline.Insert(Math::TimelineItem<Subtitle*>(22, 30, new Subtitle(dev, fontTexture, fontMap)));
         s->Set("E toca a codar uma demo!", hScale * REFERENCE_SUBTITLE_SIZE);
 
+
+        std::vector<Particle> particles[2];
+
         float time = 0.0;
         Core::Chronometer * chron = Core::CreateChronometer();
         chron->Start();
 
         audioDevice->Play();
            
+        float last_dt = 0;
+        int frame = 0;
         while (!IsTearingDown())
         {
             dev->Clear();
             dev->Viewport(0, 0, wnd->GetWidth(), wnd->GetHeight());
-
-            dev->Disable(Graph::STATE_DEPTH_TEST);
 
             dev->MatrixMode(Graph::MATRIX_PROJECTION);
             dev->Identity();
@@ -109,25 +334,42 @@ public:
 
             dev->MatrixMode(Graph::MATRIX_MODEL);
             dev->Identity();
-
-            float t = chron->GetElapsedTime() / 1000.0;
-
+                       
             dev->Enable(Graph::STATE_BLEND);
             dev->Disable(Graph::STATE_DEPTH_TEST);
             dev->BlendFunc(Graph::BLEND_SRC_ALPHA, Graph::BLEND_INV_SRC_ALPHA);
 
-            shpRender->Square(wnd->GetWidth() * 0.5-5.0, wnd->GetHeight() * 0.5-5.0, 10, 10, Math::Color4ub(255, 255, 255));
-           
-            std::list<Math::TimelineItem<Subtitle*>> res;
-            subtitlesTimeline.Get(t, &res);
+            const float t = chron->GetElapsedTime();
+            float dt = t - last_dt;
+            dt *= 5;
 
+            Canvas canvas(wnd->GetWidth(), wnd->GetHeight());
+
+            float l = (sin(t * 1000) * 0.05 + 1.0) + Math::RandomValue() * 0.0125;
+            //l *= 0.1;
+            Math::Color4f col = Math::Color4f(l, l * 0.725, l * 0.75, 1.0);
+            
+            particle->Enable();
+            particle->SetVariable4f("color", col.GetR(), col.GetG(), col.GetB(), col.GetA());
+            Particle::renderParticles(shpRender, frame, particles);
+            particle->Disable();
+
+
+            float tSecs = t / 1000.0;
+            std::list<Math::TimelineItem<Subtitle*>> res;
+            subtitlesTimeline.Get(tSecs, &res);
             ListFor(Math::TimelineItem<Subtitle*>, res, i) {
                 Subtitle * s = (*i).GetObject();
                 s->SetPosition(Math::Vec2(wnd->GetWidth() * 0.5, wnd->GetHeight() - hScale * REFERENCE_SUBTITLE_SIZE * 2.0));
-                s->Render(t, i->GetStart(), i->GetEnd() - i->GetStart());
+                s->Render(tSecs, i->GetStart(), i->GetEnd() - i->GetStart());
             }
 
             dev->PresentAll();
+
+            Particle::updateParticles(canvas, particles, frame, dt);
+
+            last_dt = t;
+            frame++;
         }
 
         audioDevice->Stop();
@@ -192,7 +434,7 @@ void Core::Application_Main(const std::vector<std::string> & CmdLine)
     int width = 1280, height = 720;
     bool fullscreen = false;
 
-    bool runDemo =  conf->Run(&width, &height, &fullscreen);
+    bool runDemo = true;// conf->Run(&width, &height, &fullscreen);
 
     SafeDelete(conf);
 
